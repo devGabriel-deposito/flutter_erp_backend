50f6d2d047e4c2aa91adc12e22929fcf3b1d18ae
gabriel <gabriel.goncal.freitas@gmail.com>
2023-10-08 23:26:44 -0300

    melhorado o código de inclusao do pagar adicionando mensagens de retorno

--- app/Http/Controllers/Financeiro/ContasPagar/IncluirController.php
+++ app/Http/Controllers/Financeiro/ContasPagar/IncluirController.php
@@ -2,21 +2,31 @@
 
 namespace App\Http\Controllers\Financeiro\ContasPagar;
 
-use App\Http\Controllers\Controller;
+use App\Http\HttpCodes;
 use App\Models\ContasPagar;
-use Illuminate\Http\Request;
+use App\Http\ResponseHandler;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\FinanceiroContasPagarInclusaoRequest;
 
 class IncluirController extends Controller
 {
-    public function incluir(Request $request, ContasPagar $contasPagar)
+    public function incluir(FinanceiroContasPagarInclusaoRequest $request, ContasPagar $contasPagar)
     {
         $id = $request->user()->id;
 
-        $contasPagar->user_id      = $id;
-        $contasPagar->valor        = $request->valor;
-        $contasPagar->documento    = $request->documento;
-        $contasPagar->observacao   = $request->observacao;
+        $contasPagar->user_id      = (int) $id;
+        $contasPagar->valor        = (float) str_replace($request->valor, ',', '.');
+        $contasPagar->documento    = (string) $request->documento;
+        $contasPagar->observacao   = (string) $request->observacao;
 
-        $contasPagar->save();
+        try {
+            $contasPagar->save();
+
+            return ResponseHandler::json([
+                'message' => 'Usuário criado com sucesso'
+            ], HttpCodes::$created);
+        } catch (\Throwable $th) {
+            return ResponseHandler::json($th, HttpCodes::$unprocessableContent);
+        }
     }
 }
